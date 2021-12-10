6d8e391cde7066afbffd0d53def1c901ae34ad4f
hannody <aljaboori@gmail.com>
2021-12-10 10:28:58 +0800

    Init

---
+++ blog/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+from django.contrib.auth import get_user_model
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(get_user_model(), on_delete=models.PROTECT)
+    bio = models.TextField(max_length=500, blank=True)
+    website = models.URLField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    class Meta:
+        ordering = ["-publish_date"]
+
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    meta_description = models.CharField(max_length=150, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    publish_date = models.DateTimeField(blank=True, null=True)
+    published = models.BooleanField(default=False)
+
+    author = models.ForeignKey(Profile, on_delete=models.PROTECT)
+    tags = models.ManyToManyField(Tag, blank=True)
+    
